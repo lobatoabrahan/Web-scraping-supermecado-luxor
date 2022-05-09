7ee2c3a85caa97bdb0e2a0d2afc603be2e135712
Abrahan Lobato <lobatoabrahan@gmail.com>
2022-05-08 21:36:19 -0400

    Definiendo variables

--- Prices scraping.py
+++ Prices scraping.py
@@ -1,3 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+
+url_alimentos = "https://www.supermercadoluxor.com/product-category/alimentos/"
+max_pages = 20
+count = 0
+req = requests.get(url_alimentos, headers={"User-Agent": "XY"})
+statusCode = req.status_code
