baaf225444e14101e7bea2dd0d65940f3181849f
Abrahan Lobato <lobatoabrahan@gmail.com>
2022-05-09 08:27:03 -0400

    Codigo para saber precios de la seccion de alimentos y acomodar variables

--- Prices scraping.py
+++ Prices scraping.py
@@ -4,6 +4,35 @@ import pandas as pd
 
 url_alimentos = "https://www.supermercadoluxor.com/product-category/alimentos/"
 max_pages = 20
-count = 0
+counter = 0
+df_alimentos = pd.DataFrame({'Titulo':[],
+                   'Precio':[]})
 req = requests.get(url_alimentos, headers={"User-Agent": "XY"})
 statusCode = req.status_code
+
+for i in range(1, max_pages):
+    
+    if i > 1:
+        url = "%sPage/%d/" % (url_alimentos, i)
+    else:
+        url = url_alimentos
+        
+    if statusCode == 200:
+        html = BeautifulSoup(req.text, "html.parser")
+        
+        entradas = html.find_all('ul', {'class': 'products columns-4'})
+        
+        for entrada in entradas:
+            listas = html.find_all('li')
+            
+            for lista in listas:
+                counter +=1
+                titulo = lista.find('span', {'class':'woocommerce-Price-amount amount'}).getText()
+                precio = lista.findfind('span', {'class':'woocommerce-Price-amount amount'}).getText()
+                
+                df_alimentos = df_alimentos.append({'Titulo': titulo,
+                                'Precio': precio},
+                               ignore_index=True)
+                
+    else:
+        break
